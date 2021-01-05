443739568b1d63a75b66c1f3328448fca562eb74
diyarfaraj <diyar.faraj@gmail.com>
2021-01-05 23:04:19 +0100

    added basic model and interface structure

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,21 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Command getCommandById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Commander/Models/Command.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Models
+{
+    public class Cammand
+    {
+        public int Id { get; set; }
+        public String HowTo { get; set; }
+
+        public String Line { get; set; }
+        public String Platform { get; set; }
+    }
+}
